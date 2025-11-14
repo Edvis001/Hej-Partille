2acfa32d8b98ae4c2f86996addcb77ed8206ea5e
Edvincraft <edvin.visser@gmail.com>
2025-11-14 18:27:14 +0100

    renamed:    runme.py -> code.py     modified movement to move the envorinment instead of the player

--- code.py
+++ code.py
@@ -2,9 +2,6 @@ import time
 import sys
 import pygame
 
-# testing git hub
-# testing again
-
 
 class Game:
     def __init__(self):
@@ -31,11 +28,11 @@ class Game:
             "data/images/background/victory.png").convert()
         self.win_screen = pygame.transform.scale(self.win_screen, (640, 640))
 
-        self.background_1 = pygame.image.load(
+        self.partille_l1_1 = pygame.image.load(
             "data/images/background/partille_1.png").convert()
-        self.background_1 = pygame.transform.scale(
-            self.background_1, (660, 318))
-        self.background_1.set_colorkey((0, 0, 0))
+        self.partille_l1_1 = pygame.transform.scale(
+            self.partille_l1_1, (660, 318))
+        self.partille_l1_1.set_colorkey((0, 0, 0))
 
         self.sad1 = pygame.image.load(
             "data/images/characters/sad1.png").convert()
@@ -50,53 +47,67 @@ class Game:
 
 # ------------------------------- Defenitions and cords ----------------------------------------#
 
-        self.player_img_pos = [290, 270]
-        self.movement = [False, False]
+        self.layer1_pos = [0, 150]
+        self.layer1_movement = [False, False]
 
-        self.sad1_collision = pygame.Rect(50, 290, 80, 130)
+        self.sad1_collision = None
 
+        # Define states
         self.interact = False
+        self.running = True
+        self.win = False
 
         self.time_passed_since_win = 0
 
 # ---------------------------------------------------------------------------------------------#
 
     def run(self):
-        running = True
-        while running:
+
+        while self.running:
             # Clear screen
             self.screen.fill((0, 100, 200))
 
-            # Draw background
-            self.screen.blit(self.background_1, (-5, 50))
+# ------------------------------- Draw layers --------------------------------#
+
+            self.screen.blit(self.partille_l1_1, self.layer1_pos)
+
+
+# ------------------------------- Hitboxes --------------------------------#
+
+            player_hitbox = pygame.Rect(
+                320, 380, self.player_img.get_width(
+                ),
+                self.player_img.get_height())
+
+            self.sad1_collision = pygame.Rect(
+                self.layer1_pos[0] + 50, self.layer1_pos[1] + 250, 80, 130)
+            # Show hitboxes for testing
+            pygame.draw.rect(self.screen, (255, 0, 0), player_hitbox, 2)
+            pygame.draw.rect(self.screen, (0, 255, 0), self.sad1_collision, 2)
 
 # ------------------------------- Player movement and interactions --------------------------------#
 
-            win = False
-            player_hitbox = pygame.Rect(
-                self.player_img_pos[0], self.player_img_pos[1], self.player_img.get_width(
-                ),
-                self.player_img.get_height())
             if player_hitbox.colliderect(self.sad1_collision) and self.interact is True:
-                win = True
+                self.win = True
             elif player_hitbox.colliderect(self.sad1_collision) is True:
                 self.screen.blit(self.tutorial, (0, 270))
                 self.screen.blit(self.sad1, self.sad1_collision)
             else:
                 self.screen.blit(self.sad1, self.sad1_collision)
 
-            self.player_img_pos[0] += (self.movement[1] -
-                                       self.movement[0]) * -5
-            if not any(self.movement):
-                self.screen.blit(self.player_img, self.player_img_pos)
+            self.layer1_pos[0] += (self.layer1_movement[1] -
+                                   self.layer1_movement[0]) * 5
+            if not any(self.layer1_movement):
+                self.screen.blit(
+                    self.player_img, player_hitbox.topleft)
             else:
-                if self.movement[0]:
-                    self.screen.blit(self.player_moving, self.player_img_pos)
+                if self.layer1_movement[0]:
+                    self.screen.blit(self.player_moving, player_hitbox.topleft)
                 else:
                     flipped_player_moving = pygame.transform.flip(
                         self.player_moving, True, False)
                     self.screen.blit(flipped_player_moving,
-                                     self.player_img_pos)
+                                     (player_hitbox.topleft))
 
 # ------------------------------- Inputs handling --------------------------------#
 
@@ -106,23 +117,27 @@ class Game:
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        self.movement[0] = True
+                        self.layer1_movement[0] = True
                     if event.key == pygame.K_LEFT:
-                        self.movement[1] = True
+                        self.layer1_movement[1] = True
                     if event.key == pygame.K_e:
                         self.interact = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
-                        self.movement[0] = False
+                        self.layer1_movement[0] = False
                     if event.key == pygame.K_LEFT:
-                        self.movement[1] = False
+                        self.layer1_movement[1] = False
 
-            if win is True:
+# ------------------------------- Win condition --------------------------------#
+
+            if self.win is True:
                 self.screen.blit(self.win_screen, (0, 0))
-                self.movement = [False, False]
+                self.layer1_movement = [False, False]
                 self.time_passed_since_win += self.clock.get_time()
                 if self.time_passed_since_win > 3000:
-                    running = False
+                    self.running = False
+
+# ---------------------------------------------------------------------------------------------#
 
             pygame.display.update()
             self.clock.tick(60)
